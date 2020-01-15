b19cccad23972ea3731408e5edd55522e32e742f
veronkrot <veronkrotenko@gmail.com>
2020-01-15 20:37:39 +0200

    added tetrominoes

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
+import { createStage } from '../gameHelpers'
 
 const Tetris = () => {
 
     return (
         <div>
+            <Stage stage={createStage()} />
             <aside>
                 <div>
                     <Display text="Score"/>
